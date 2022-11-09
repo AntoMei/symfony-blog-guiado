feca7f93376c3f24e7dde12e34e984a2b66811aa
AntoMei <antonio.meirosu444@gmail.com>
2022-11-09 08:33:43 +0100

    Finalizado hasta el punto 4.4.5

--- symfony-blog-3/src/Controller/AdminController.php
+++ symfony-blog-3/src/Controller/AdminController.php
@@ -11,6 +11,9 @@ use App\Entity\Category;
 use App\Form\CategoryFormType;
 use App\Entity\Image;
 use App\Form\ImageFormType;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AdminController extends AbstractController
@@ -29,7 +32,33 @@ class AdminController extends AbstractController
         $form = $this->createForm(ImageFormType::class, $image);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $image = $form->getData();    
+            $file = $form->get('File')->getData();
+            if ($file) {
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+        
+                // Move the file to the directory where images are stored
+                try {
+        
+                    $file->move(
+                        $this->getParameter('images_directory'), $newFilename
+                    );
+                    $filesystem = new Filesystem();
+                    $filesystem->copy(
+                        $this->getParameter('images_directory') . '/'. $newFilename, 
+                        $this->getParameter('portfolio_directory') . '/'.  $newFilename, true);
+        
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+        
+                // updates the 'file$filename' property to store the PDF file name
+                // instead of its contents
+                $image->setFile($newFilename);
+            }
+            $image = $form->getData();     
             $entityManager = $doctrine->getManager();    
             $entityManager->persist($image);
             $entityManager->flush();
